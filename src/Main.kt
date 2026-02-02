d15eeaddc7698e8faec9a7dbd2202e8220ff4822
Ilya Smolin <ilyasmolin9@gmail.com>
2026-02-02 20:47:18 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+
+    var Test: String? = "sdfsdf"
+
+    Test = null
+
+    println(Test)
+
+
+    //проверяем отсутствие элемента на страничке
+
+
+
+
+}
